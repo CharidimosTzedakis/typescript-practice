f809be4729231682cb8b595dd46613e7c95adadf
Charidimos Tzedakis <harris.jedakis@gmail.com>
2024-05-25 15:55:04 +0300

    charter 2 and 3 fixes for compilation

--- src/charter-2/index.ts
+++ src/charter-2/index.ts
@@ -1,57 +1,55 @@
-console.log('Hello Typescript!')
+console.log("Hello Typescript!");
 
 let a = 1 + 2;
-let b = a + 3
+let b = a + 3;
 let c = {
-    apple: a,
-    banana: b
-}
-let d = 1? "s" : null;
+  apple: a,
+  banana: b,
+};
+let d = 1 ? "s" : null;
 
 // let d = c.apple * "s"
 
-let operation = (param: string|undefined) => {
+let operation = (param: string | undefined) => {
+  if (typeof param === "undefined") {
+    return 0;
+  }
 
- if (typeof (param) === 'undefined') {
-        return 0;
-    }
-
-    return param;
+  return param;
 };
 
 // ----------------------------------------------------------  //
 function defaultIt(s: MaybeString): string {
-    if (typeof s.content === "string") {
-        return s.content;
-    }
+  if (typeof s.content === "string") {
+    return s.content;
+  }
 
-    return "";
+  return "";
 }
 
 class MaybeString {
-    content: string|void;
+  content: string | void;
 
-    constructor() {
-        this.content = ''
-    }
+  constructor() {
+    this.content = "";
+  }
 }
 
 // ---------------------------------------------------------  //
 
 interface AType {
-    type: 'A'
-    message: string
+  type: "A";
+  message: string;
 }
 
 interface BCType {
-    type: 'B' | 'C'
+  type: "B" | "C";
 }
 
-type Test = AType | BCType
+type Test = AType | BCType;
 
-function doSomething(test: Test) {
-    if (test.type !== 'B' && test.type !== 'C') {
-        test.message
-    }
-}
-
+// function doSomething(test: Test) {
+//     if (test.type !== 'B' && test.type !== 'C') {
+//         test.message
+//     }
+// }
--- src/charter-3/index.ts
+++ src/charter-3/index.ts
@@ -1,90 +1,99 @@
 let foo: unknown = 32;
-let bar = foo ===123;
+let bar = foo === 123;
 // let z = foo + bar;
 
-if (typeof foo === 'number') {
-    const a1 = foo +100;
+if (typeof foo === "number") {
+  const a1 = foo + 100;
 }
 
 let a3: true = true;
 // let a4: true = false;
 
-
 // ---------------------------------------------------- //
 
 let a6: object = {
-    b: 'x'
-}
+  b: "x",
+};
 
 // a6.b
 
 let a7 = {
-    c: {
-        d: 'f'
-    }
-}
+  c: {
+    d: "f",
+  },
+};
 
 let a8 = {
-    b: 'x'
-}
+  b: "x",
+};
 
 // -------------------- Objects -------------------------- //
 let person: {
-    firstName: string,
-    lastName: string
+  firstName: string;
+  lastName: string;
 } = {
-    firstName: 'john',
-    lastName: 'barrowman'
-}
+  firstName: "john",
+  lastName: "barrowman",
+};
 
 class Person {
-    constructor(public firstName: string, public lastName: string) {}
+  constructor(
+    public firstName: string,
+    public lastName: string,
+  ) {}
 }
 
-person = new Person ('matt', 'smith');
+person = new Person("matt", "smith");
 
-let a9 : {b: number}
-a9 = {}
-a9 = {
-    b: 1,
-    c: 2
-}
+let a9: { b: number };
+// a9 = {};
+// a9 = {
+//   b: 1,
+//   c: 2,
+// };
 
 let a10: {
-    b: number,
-    c?: string,
-    [key: number]: boolean
-}
+  b: number;
+  c?: string;
+  [key: number]: boolean;
+};
 
 a10 = {
-    b: 1,
-    "1": true
-}
+  b: 1,
+  "1": true,
+};
 
-let airplaneSeatingAssignments : {
-    [seatNumber: string]: string
+let airplaneSeatingAssignments: {
+  [seatNumber: string]: string;
 } = {
-    '34D': 'Borris Cherny',
-    '34E': 'Bill Gates'
-}
+  "34D": "Borris Cherny",
+  "34E": "Bill Gates",
+};
 
 let user: {
-    readonly firstName: string
+  readonly firstName: string;
 } = {
-    firstName: 'abby'
-}
+  firstName: "abby",
+};
 
-user.firstName = 'abbey with an e';
+// user.firstName = "abbey with an e";
 
+let danger: {};
+danger = {};
+danger = { x: 1 };
+danger = [];
+danger = 2;
 
-let danger: {}
-danger = {}
-danger = {x: 1}
-danger = []
-danger = 2
-
-let a11: {} = {toString(){return 3}}
-let a12: Object = {toString(){return 3}}
+let a11: {} = {
+  toString() {
+    return 3;
+  },
+};
+// let a12: Object = {
+//   toString() {
+//     return 3;
+//   },
+// };
 
 // ------- Union and Intersections ----------- //
 
@@ -124,82 +133,75 @@ let a12: Object = {toString(){return 3}}
 //     console.log(pet.barks);
 // }
 
-
 interface Cat {
-    weight: number;
-    whiskers: number;
+  weight: number;
+  whiskers: number;
 }
 interface Dog {
-    weight: number;
-    friendly: boolean;
+  weight: number;
+  friendly: boolean;
 }
 let animal: Dog | Cat;
 
 animal = {
-    weight: 1,
-    whiskers: 1,
-    friendly: false,
-}
+  weight: 1,
+  whiskers: 1,
+  friendly: false,
+};
 
 // ------ arrays -------- //
-let c1: string [];
-c1 = ['a'];
+let c1: string[];
+c1 = ["a"];
 
-let c2 = [1, 2, 'test']
-c2.map(_ => {
-  if (typeof _ === 'number') {
-    return _ * 3
+let c2 = [1, 2, "test"];
+c2.map((_) => {
+  if (typeof _ === "number") {
+    return _ * 3;
   }
   return _.toUpperCase();
-})
+});
 
 // ----- tuples ------ //
-let trainFares: [number, number?][] = [
-    [3.75],
-    [8.25, 7.70],
-    [10.50]
-]
+let trainFares: [number, number?][] = [[3.75], [8.25, 7.7], [10.5]];
 let moreTrainFares: ([number] | [number, number])[];
-let friends: [string, ...string[]] = ['Sara', 'Tali', 'Chloe', 'Claire']
+let friends: [string, ...string[]] = ["Sara", "Tali", "Chloe", "Claire"];
 
-let as: readonly number [] ;
-as = [2]
-as = as.map(_ => 2)
+let as: readonly number[];
+as = [2];
+as = as.map((_) => 2);
 
 // ----
 enum Status {
-    Todo,
-    InProgress,
-    Complete
+  Todo,
+  InProgress,
+  Complete,
 }
 
 enum Language {
-    English = 100,
-    Spanish = 200 + 300,
-    Russian
+  English = 100,
+  Spanish = 200 + 300,
+  Russian,
 }
 
 let myFirstLanguage = Language.Russian;
-let mySecondLanguage = Language['English'];
+let mySecondLanguage = Language["English"];
 
 const enum Color {
-    Red = '#c10000',
-    Blue = '#007ac1',
-    Pink = '0xc10050',
-    White = 255,
+  Red = "#c10000",
+  Blue = "#007ac1",
+  Pink = "0xc10050",
+  White = 255,
 }
 
-let c5 = Color[6]
-
+// let c5 = Color[6];
 
 // --------- exercises ------- //
 let aa = 1042;
-let bb = 'apples and oranges'
-const c = 'pineapples';
+let bb = "apples and oranges";
+const cc = "pineapples";
 let dd = [true, true, false];
-let ee = {type: 'focus'};
+let ee = { type: "focus" };
 let f = [1, false];
-const g= [3];
+const g = [3];
 let h = null;
 const i = null;
-
