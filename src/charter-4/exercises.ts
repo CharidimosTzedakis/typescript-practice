0cd7fc2f31973bf39e0cec640a3ccfa5b2f4248f
Charidimos Tzedakis <harris.jedakis@gmail.com>
2024-08-25 14:24:44 +0300

    comment out compile error

--- src/charter-4/exercises.ts
+++ src/charter-4/exercises.ts
@@ -128,5 +128,5 @@ function is<T>(a: T, ...b: [T, ...T[]]): boolean {
 console.log(is("string", "otherstring"));
 console.log(is(true, false));
 console.log(is(42, 42));
-console.log(is(10, "foo"));
+// console.log(is(10, "foo"));
 console.log(is([1], [1, 2], [1, 2, 3]));
