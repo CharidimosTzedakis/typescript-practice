36a4e81e3037ba4dc91bad90a093a954ca82b279
Charidimos Tzedakis <harris.jedakis@gmail.com>
2025-01-17 01:47:46 +0200

    charter 5 chess engine

--- src/charter-5/chessEngine.ts
+++ src/charter-5/chessEngine.ts
@@ -9,6 +9,13 @@ class Position {
     private file: File,
     private rank: Rank,
   ) {}
+
+  distanceFrom(position: Position) {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0)),
+    };
+  }
 }
 
 abstract class Piece {
@@ -20,11 +27,43 @@ abstract class Piece {
   ) {
     this.position = new Position(file, rank);
   }
+
+  moveTo(position: Position) {
+    this.position = position;
+  }
+
+  abstract canMoveTo(position: Position): boolean;
 }
 
-// class Game {}
-//
-// class King extends Piece {}
+class King extends Piece {
+  canMoveTo(position: Position): boolean {
+    const distance = this.position.distanceFrom(position);
+    return distance.rank < 2 && distance.file < 2;
+  }
+}
+
+class Game {
+  private pieces = Game.makePieces();
+
+  private static makePieces() {
+    return [
+      // Kings
+      new King("White", "E", 1),
+      new King("Black", "E", 8),
+
+      // Queens
+      // new Queen("White", "D", 1),
+      // new Queen("Black", "D", 8),
+
+      // Bishops
+      // new Bishop("White", "C", 1),
+      // new Bishop("White", "F", 1),
+      // new Bishop("Black", "C", 8),
+      // new Bishop("Black", "F", 8),
+    ];
+  }
+}
+
 // class Queen extends Piece {}
 // class Bishop extends Piece {}
 // class Knight extends Piece {}
