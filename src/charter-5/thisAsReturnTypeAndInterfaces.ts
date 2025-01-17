de376d83979faa2c9494dc4f95ce9d8b332ae59d
Charidimos Tzedakis <harris.jedakis@gmail.com>
2025-01-17 14:08:08 +0200

    custom type guards and declaration merging

--- src/charter-5/thisAsReturnTypeAndInterfaces.ts
+++ src/charter-5/thisAsReturnTypeAndInterfaces.ts
@@ -47,3 +47,58 @@ const takis: Person = {
   name: "takis",
   age: 25,
 };
+
+interface AType {
+  type: "A";
+  message: string;
+}
+
+interface BCType {
+  type: "B" | "C";
+}
+
+type Test = AType | BCType;
+
+const c: Test = {
+  type: "A",
+  message: "hello",
+};
+
+function doSomething(test: Test) {
+  if (test.type !== "B" && test.type !== "C") {
+    test.message;
+  }
+}
+
+function handleTest(test: Test) {
+  if (test.type === "A") {
+    // Narrowed to 'AType'
+    console.log(test.message); // Valid because 'message' exists only in AType
+  } else {
+    // Narrowed to 'BCType' as test.type is "B" or "C"
+    console.log("No message property for B or C types.");
+  }
+}
+
+function isString(test: any): test is string {
+  return typeof test === "string";
+}
+
+function example(foo: string | number) {
+  if (isString(foo)) {
+    console.log("it is a string" + foo);
+    console.log(foo.length); // string function
+  }
+}
+
+interface User<Age> {
+  age: Age;
+}
+
+interface User<Age> {
+  age: Age;
+}
+
+const user: User<25> = {
+  age: 25,
+};
