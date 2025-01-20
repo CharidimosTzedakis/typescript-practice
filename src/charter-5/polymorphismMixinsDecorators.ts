2add19d362b645ce6ee935b88b3f4107f8d1a7af
Charidimos Tzedakis <harris.jedakis@gmail.com>
2025-01-20 15:23:29 +0200

    Refactor MyMap class to improve API consistency

--- src/charter-5/polymorphismMixinsDecorators.ts
+++ src/charter-5/polymorphismMixinsDecorators.ts
@@ -1,6 +1,5 @@
 class MyMap<K, V> {
   private _map: Map<K, V> = new Map();
-  forEach = this._map.forEach;
 
   constructor(initialKey?: K, initialValue?: V) {
     if (initialKey && initialValue) {
@@ -17,6 +16,13 @@ class MyMap<K, V> {
     this._map.set(key, value);
   }
 
+  forEach(
+    callbackfn: (value: V, key: K, map: Map<K, V>) => void,
+    thisArg?: unknown,
+  ): void {
+    this._map.forEach(callbackfn, thisArg);
+  }
+
   merge<K1, V1>(map: MyMap<K1, V1>): MyMap<K | K1, V | V1> {
     const mergedMap = new MyMap<K | K1, V | V1>();
     this._map.forEach((value, key) => {
@@ -29,14 +35,16 @@ class MyMap<K, V> {
     return mergedMap;
   }
 
-  static of<K, V>(k: K, v: V): MyMap<K, V> {}
+  static of<K, V>(k: K, v: V): MyMap<K, V> {
+    return new MyMap<K, V>(k, v);
+  }
 }
 
 const mapVar = new MyMap(1, "one");
 mapVar.set(2, "two");
 mapVar.set(3, "three");
 
-const mapVar2 = new MyMap(4, "four");
+const mapVar2 = MyMap.of(4, "four");
 mapVar2.set(2, "twoAltered");
 
 const result = mapVar.merge(mapVar2);
